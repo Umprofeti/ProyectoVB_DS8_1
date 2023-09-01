78ab4d366169c1df3153b77950c9e58dd42cecd8
Jonathan Rodríguez <vansthewall16@gmail.com>
2023-09-01 14:16:38 -0500

    Se realizan correcciones a la solucion y al diseño del formulario

---
+++ WinFormsApp2/CalculoPlanilla.Designer.vb
@@ -0,0 +1,578 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CalculoPlanilla
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label3 = New Label()
+        TextBox2 = New TextBox()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        TextBox8 = New TextBox()
+        TextBox9 = New TextBox()
+        TextBox10 = New TextBox()
+        TextBox11 = New TextBox()
+        TextBox12 = New TextBox()
+        TextBox13 = New TextBox()
+        Label18 = New Label()
+        Panel = New Panel()
+        TextBox7 = New TextBox()
+        Label8 = New Label()
+        CheckBox2 = New CheckBox()
+        CheckBox1 = New CheckBox()
+        Label4 = New Label()
+        TextBox6 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox3 = New TextBox()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        Nombre = New Label()
+        TextBox1 = New TextBox()
+        ComboBox1 = New ComboBox()
+        Label2 = New Label()
+        Label1 = New Label()
+        Panel1 = New Panel()
+        Button1 = New Button()
+        I_ISR = New TextBox()
+        Label19 = New Label()
+        I_SE = New TextBox()
+        Label16 = New Label()
+        I_SS = New TextBox()
+        Label15 = New Label()
+        I_SB = New TextBox()
+        Label17 = New Label()
+        Label20 = New Label()
+        I_SN = New TextBox()
+        Panel.SuspendLayout()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(264, 18)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(47, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Asiento"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(264, 36)
+        TextBox2.MaxLength = 5
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(72, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(64, 333)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(96, 15)
+        Label9.TabIndex = 19
+        Label9.Text = "Horas trabajados"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(64, 362)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(80, 15)
+        Label10.TabIndex = 20
+        Label10.Text = "Salario x Hora"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(68, 391)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(67, 15)
+        Label11.TabIndex = 21
+        Label11.Text = "Horas Extra"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(304, 361)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(72, 15)
+        Label12.TabIndex = 22
+        Label12.Text = "Descuento 2"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Location = New Point(304, 390)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(72, 15)
+        Label13.TabIndex = 23
+        Label13.Text = "Descuento 3"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Location = New Point(304, 332)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(72, 15)
+        Label14.TabIndex = 24
+        Label14.Text = "Descuento 1"
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(174, 324)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(100, 23)
+        TextBox8.TabIndex = 25
+        ' 
+        ' TextBox9
+        ' 
+        TextBox9.Location = New Point(382, 324)
+        TextBox9.Name = "TextBox9"
+        TextBox9.Size = New Size(100, 23)
+        TextBox9.TabIndex = 26
+        ' 
+        ' TextBox10
+        ' 
+        TextBox10.Location = New Point(174, 353)
+        TextBox10.Name = "TextBox10"
+        TextBox10.Size = New Size(100, 23)
+        TextBox10.TabIndex = 27
+        ' 
+        ' TextBox11
+        ' 
+        TextBox11.Location = New Point(174, 382)
+        TextBox11.Name = "TextBox11"
+        TextBox11.Size = New Size(100, 23)
+        TextBox11.TabIndex = 28
+        ' 
+        ' TextBox12
+        ' 
+        TextBox12.Location = New Point(382, 382)
+        TextBox12.Name = "TextBox12"
+        TextBox12.Size = New Size(100, 23)
+        TextBox12.TabIndex = 29
+        ' 
+        ' TextBox13
+        ' 
+        TextBox13.Location = New Point(382, 353)
+        TextBox13.Name = "TextBox13"
+        TextBox13.Size = New Size(100, 23)
+        TextBox13.TabIndex = 30
+        ' 
+        ' Label18
+        ' 
+        Label18.AutoSize = True
+        Label18.BackColor = SystemColors.ActiveCaption
+        Label18.Location = New Point(27, 407)
+        Label18.Name = "Label18"
+        Label18.Size = New Size(52, 15)
+        Label18.TabIndex = 41
+        Label18.Text = "Calculos"
+        ' 
+        ' Panel
+        ' 
+        Panel.BackColor = SystemColors.ActiveCaption
+        Panel.BorderStyle = BorderStyle.Fixed3D
+        Panel.Controls.Add(TextBox7)
+        Panel.Controls.Add(Label8)
+        Panel.Controls.Add(CheckBox2)
+        Panel.Controls.Add(CheckBox1)
+        Panel.Controls.Add(Label4)
+        Panel.Controls.Add(TextBox6)
+        Panel.Controls.Add(TextBox5)
+        Panel.Controls.Add(TextBox4)
+        Panel.Controls.Add(TextBox3)
+        Panel.Controls.Add(Label7)
+        Panel.Controls.Add(Label6)
+        Panel.Controls.Add(Label5)
+        Panel.Controls.Add(Nombre)
+        Panel.Controls.Add(TextBox1)
+        Panel.Controls.Add(ComboBox1)
+        Panel.Controls.Add(Label2)
+        Panel.Controls.Add(Label1)
+        Panel.Controls.Add(Label3)
+        Panel.Controls.Add(TextBox2)
+        Panel.ForeColor = Color.Black
+        Panel.Location = New Point(38, 23)
+        Panel.Name = "Panel"
+        Panel.RightToLeft = RightToLeft.No
+        Panel.Size = New Size(459, 283)
+        Panel.TabIndex = 45
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(140, 190)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(100, 23)
+        TextBox7.TabIndex = 30
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(140, 172)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(90, 15)
+        Label8.TabIndex = 29
+        Label8.Text = "Apellido casada"
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(9, 228)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(42, 19)
+        CheckBox2.TabIndex = 28
+        CheckBox2.Text = "No"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(10, 202)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(35, 19)
+        CheckBox1.TabIndex = 27
+        CheckBox1.Text = "Si"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(7, 172)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(69, 15)
+        Label4.TabIndex = 26
+        Label4.Text = "Es casado/a"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(163, 135)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(100, 23)
+        TextBox6.TabIndex = 25
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(163, 91)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(100, 23)
+        TextBox5.TabIndex = 24
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(7, 135)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 23
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(7, 91)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 22
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(160, 117)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(60, 15)
+        Label7.TabIndex = 21
+        Label7.Text = "Apellido 2"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(163, 73)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(54, 15)
+        Label6.TabIndex = 20
+        Label6.Text = "Apellido "
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(7, 117)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(60, 15)
+        Label5.TabIndex = 19
+        Label5.Text = "Nombre 2"
+        ' 
+        ' Nombre
+        ' 
+        Nombre.AutoSize = True
+        Nombre.Location = New Point(7, 73)
+        Nombre.Name = "Nombre"
+        Nombre.Size = New Size(51, 15)
+        Nombre.TabIndex = 18
+        Nombre.Text = "Nombre"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(166, 36)
+        TextBox1.MaxLength = 4
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(74, 23)
+        TextBox1.TabIndex = 17
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.ItemHeight = 15
+        ComboBox1.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "E", "PE"})
+        ComboBox1.Location = New Point(7, 36)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 16
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(163, 18)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(37, 15)
+        Label2.TabIndex = 15
+        Label2.Text = "Tomo"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(7, 18)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 15)
+        Label1.TabIndex = 14
+        Label1.Text = "Prefijo"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = SystemColors.ActiveCaption
+        Panel1.BorderStyle = BorderStyle.Fixed3D
+        Panel1.Controls.Add(I_SN)
+        Panel1.Controls.Add(Label20)
+        Panel1.Controls.Add(Button1)
+        Panel1.Controls.Add(I_ISR)
+        Panel1.Controls.Add(Label19)
+        Panel1.Controls.Add(I_SE)
+        Panel1.Controls.Add(Label16)
+        Panel1.Controls.Add(I_SS)
+        Panel1.Controls.Add(Label15)
+        Panel1.Controls.Add(I_SB)
+        Panel1.Controls.Add(Label17)
+        Panel1.Location = New Point(38, 420)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(459, 146)
+        Panel1.TabIndex = 46
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(332, 92)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(118, 47)
+        Button1.TabIndex = 53
+        Button1.Text = "Calcular"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' I_ISR
+        ' 
+        I_ISR.Location = New Point(44, 76)
+        I_ISR.Name = "I_ISR"
+        I_ISR.Size = New Size(51, 23)
+        I_ISR.TabIndex = 52
+        ' 
+        ' Label19
+        ' 
+        Label19.AutoSize = True
+        Label19.Location = New Point(11, 79)
+        Label19.Name = "Label19"
+        Label19.Size = New Size(29, 15)
+        Label19.TabIndex = 51
+        Label19.Text = "I.S.R"
+        ' 
+        ' I_SE
+        ' 
+        I_SE.Location = New Point(44, 105)
+        I_SE.Name = "I_SE"
+        I_SE.Size = New Size(51, 23)
+        I_SE.TabIndex = 50
+        ' 
+        ' Label16
+        ' 
+        Label16.AutoSize = True
+        Label16.Location = New Point(12, 108)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(22, 15)
+        Label16.TabIndex = 49
+        Label16.Text = "S.E"
+        ' 
+        ' I_SS
+        ' 
+        I_SS.Location = New Point(44, 42)
+        I_SS.Name = "I_SS"
+        I_SS.Size = New Size(51, 23)
+        I_SS.TabIndex = 48
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Location = New Point(11, 45)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(22, 15)
+        Label15.TabIndex = 47
+        Label15.Text = "S.S"
+        ' 
+        ' I_SB
+        ' 
+        I_SB.Location = New Point(44, 13)
+        I_SB.Name = "I_SB"
+        I_SB.Size = New Size(51, 23)
+        I_SB.TabIndex = 46
+        ' 
+        ' Label17
+        ' 
+        Label17.AutoSize = True
+        Label17.Location = New Point(11, 16)
+        Label17.Name = "Label17"
+        Label17.Size = New Size(23, 15)
+        Label17.TabIndex = 45
+        Label17.Text = "S.B"
+        ' 
+        ' Label20
+        ' 
+        Label20.AutoSize = True
+        Label20.Location = New Point(134, 18)
+        Label20.Name = "Label20"
+        Label20.Size = New Size(25, 15)
+        Label20.TabIndex = 54
+        Label20.Text = "S.N"
+        ' 
+        ' I_SN
+        ' 
+        I_SN.Location = New Point(163, 13)
+        I_SN.Name = "I_SN"
+        I_SN.Size = New Size(53, 23)
+        I_SN.TabIndex = 55
+        ' 
+        ' CalculoPlanilla
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(554, 578)
+        Controls.Add(Panel1)
+        Controls.Add(Panel)
+        Controls.Add(Label18)
+        Controls.Add(TextBox13)
+        Controls.Add(TextBox12)
+        Controls.Add(TextBox11)
+        Controls.Add(TextBox10)
+        Controls.Add(TextBox9)
+        Controls.Add(TextBox8)
+        Controls.Add(Label14)
+        Controls.Add(Label13)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Name = "CalculoPlanilla"
+        Text = "Form1"
+        Panel.ResumeLayout(False)
+        Panel.PerformLayout()
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox7_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents TextBox13 As TextBox
+
+    Private Sub TextBox15_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Friend WithEvents Label18 As Label
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Friend WithEvents Panel As Panel
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Nombre As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents I_ISR As TextBox
+    Friend WithEvents Label19 As Label
+    Friend WithEvents I_SE As TextBox
+    Friend WithEvents Label16 As Label
+    Friend WithEvents I_SS As TextBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents I_SB As TextBox
+    Friend WithEvents Label17 As Label
+    Friend WithEvents I_SN As TextBox
+    Friend WithEvents Label20 As Label
+End Class
---
+++ WinFormsApp2/CalculoPlanilla.vb
@@ -0,0 +1,8 @@
+﻿Class CalculoPlanilla
+    Private Sub Label17_Click(sender As Object, e As EventArgs) Handles Label17.Click
+
+    End Sub
+
+    Private Sub I_SB_TextChanged(sender As Object, e As EventArgs) Handles I_SB.TextChanged
+    End Sub
+End Class
